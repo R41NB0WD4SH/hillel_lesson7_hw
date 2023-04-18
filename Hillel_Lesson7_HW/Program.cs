ab2f1c3419d401aa6b8d240958ee660b035473e2
R41NB0WD4SH <116415348+R41NB0WD4SH@users.noreply.github.com>
2023-04-18 03:43:34 +0300

    1

---
+++ Hillel_Lesson7_HW/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Hillel_Lesson7_HW
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int[] numbers = new int[100000];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i;
+                // Console.WriteLine(i);
+            }
+
+            List<int> filteredRoots = new List<int>();
+
+
+            Filter filter = new Filter(FilterNumbers);
+            
+            
+
+
+
+
+            Console.WriteLine();
+
+
+
+        }
+
+        public delegate void Filter(int[] numbers, int start, int end, List<int> filteredNumbers);
+
+        public static void FilterNumbers(int[] numbers, int start, int end, List<int> filteredNumbers)
+        {
+            for (int i = start; i < end; i++)
+            {
+                if (Math.Sqrt(numbers[i]) % 1 == 0)
+                {
+                    filteredNumbers.Add(numbers[i]);
+                }
+            }
+        }
+
+
+
+    }
+}
